5cc7b0338ac0244ea53eef62dffc4b08d1a74610
Ramonqlee <ramonqlee1980@gmail.com>
2018-11-28 21:26:23 +0800

    去掉看门狗

--- AIGashapon/mywd.lua
+++ AIGashapon/mywd.lua
@@ -14,30 +14,11 @@ local TAG = "MYWD"
 mywd={}
 
 local function init()
-	if wd and "function"== type(wd) then
-		return
-	end
 
-	wd = pins.setup(pio.P2_0,wd_val)
-	LogUtil.d(TAG,"feeddog init type(wd) ="..type(wd))
 end
 
 function mywd.feed()
-	init()
-	if not wd or "function"~= type(wd) then
-		return
-	end
-
-	LogUtil.d(TAG,"feeddog wd_val = "..wd_val)
-	wd(wd_val)
-
-	--喂完立即翻转，为下一次做准备
-	if 0 == wd_val then 
-		wd_val = 1
-	else
-		wd_val = 0
-	end
-
+	
 end
 
 
